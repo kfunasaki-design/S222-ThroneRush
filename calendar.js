653c8481c850409c64ab90b599b4297ea6b486c4
kfunasaki-design <kfunasaki@frontes.co.jp>
2026-09-11 02:55:03 +0900

    Adjust button positioning calculations in calendar.js

--- calendar.js
+++ calendar.js
@@ -1143,7 +1143,7 @@ function renderCalendar() {
         (
           lanes.length
           *
-          20
+          22
         )
         +
         10
@@ -1524,7 +1524,7 @@ function createSchedule(
     `calc(${segment.endColumn - segment.startColumn + 1} * (100% / 7) - 8px)`;
 
   button.style.top =
-    `${38 + laneIndex * 20}px`;
+    `${38 + laneIndex * 22}px`;
 
   button.addEventListener(
     "click",
